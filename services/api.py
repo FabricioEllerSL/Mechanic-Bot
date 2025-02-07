90cc9d8cb1c534eec011d92e9b6d7ff24ea10007
FabricioEllerSL <fabricioellersl@gmail.com>
2025-02-07 15:41:26 -0300

    :sparkles: feat: added api to access bot responses

---
+++ services/api.py
@@ -0,0 +1,16 @@
+from bot import *
+from flask import Flask
+
+chatbot = iniciar()
+api_bot = Flask(__name__)
+
+@api_bot.route("/resposta/<mensagem>")
+def get_resposta(mensagem):
+    resposta = chatbot.get_response(mensagem)
+
+    print(resposta)
+
+    return resposta.text
+
+if __name__ == "__main__":
+    api_bot.run()
